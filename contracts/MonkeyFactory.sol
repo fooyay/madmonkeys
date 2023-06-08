7040c021d063dcc35d863014f6da14ed05b4c1da
Sean Coates <sean.coates+fooyay@gmail.com>
2023-06-08 16:02:23 -0400

    make a createMonkey function

--- contracts/MonkeyFactory.sol
+++ contracts/MonkeyFactory.sol
@@ -1,6 +1,16 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
-pragma solidity >=0.8.0 <0.9.0;
+pragma solidity ^0.8.0;
 
 contract MonkeyFactory {
     uint dnaDigits = 16;
+    uint dnaModulus = 10 ** dnaDigits;
+
+    struct Monkey {
+        string name;
+        uint dna;
+    }
+    Monkey[] public monkeys;
+
+    function createMonkey(string memory _name, uint _dna) public {}
+
 }
