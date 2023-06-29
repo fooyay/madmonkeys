99366b5e65261a28998ee14bcc487903fa130d59
Sean Coates <sean.coates+fooyay@gmail.com>
2023-06-28 21:05:53 -0400

    start work on PriceConsumerV3 contract, an oracle to get prices

---
+++ contracts/PriceConsumerV3.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.0;
+
+import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol"
+
+contract PriceConsumerV3 {}
