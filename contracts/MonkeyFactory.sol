dec60a53ef8070195a7c5f5fb92402af036ed2d3
Sean Coates <sean.coates+fooyay@gmail.com>
2023-07-31 09:18:45 -0400

    disable chainlink for now

--- contracts/MonkeyFactory.sol
+++ contracts/MonkeyFactory.sol
@@ -5,6 +5,11 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
+// for random numbers using VRF - disabled for now
+// import "@chainlink/contracts/src/v0.8/VRFConsumerBase.sol";
+
+// disable VRF for now
+// contract MonkeyFactory is Ownable, VRFConsumerBase {
 contract MonkeyFactory is Ownable {
   using SafeMath for uint256;
 
@@ -13,6 +18,13 @@ contract MonkeyFactory is Ownable {
   uint dnaDigits = 16;
   uint dnaModulus = 10 ** dnaDigits;
   uint cooldownTime = 1 days;
+
+  // Chainlink random number generator variables
+  /* disable Chainlink for now
+  bytes32 public keyHash;
+  uint256 public fee;
+  uint256 public randomResult;
+  */
 
   struct Monkey {
     string name;
@@ -23,6 +35,24 @@ contract MonkeyFactory is Ownable {
     uint16 lossCount;
   }
   Monkey[] public monkeys;
+
+  // Main net:
+  // VRF 0x271682DEB8C4E0901D1a1550aD2e64D568E69909
+  // LINK 0x514910771AF9Ca656af840dff83E8264EcF986CA
+  // Sepolia:
+  // VRF 0x8103B0A8A00be2DDC778e6e7eaa21791Cd364625
+  // LINK 0x779877A7B0D9E8603169DdbD7836e478b4624789
+  /* disable Chainlink for now
+  constructor()
+    VRFConsumerBase(
+      0x8103B0A8A00be2DDC778e6e7eaa21791Cd364625,
+      0x779877A7B0D9E8603169DdbD7836e478b4624789
+    )
+  {
+    keyHash = 0x2ed0feb3e7fd2022120aa84fab1945545a9f2ffc9076fd6156fa96eaff4c1311;
+    fee = 100000000000000000;
+  }
+  */
 
   mapping(uint => address) public monkeyToOwner;
   mapping(address => uint) ownerMonkeyCount;
@@ -37,6 +67,20 @@ contract MonkeyFactory is Ownable {
     emit NewMonkey(id, _name, _dna);
   }
 
+  /* disable VRF for now
+  function getRandomNumber() public returns (bytes32 requestId) {
+    return requestRandomness(keyHash, fee);
+  }
+
+  function fulfillRandomness(
+    bytes32 requestId,
+    uint256 randomness
+  ) internal override {
+    randomResult = randomness;
+  }
+  */
+
+  // if we switch to using VRF, comment out this function or modify it to the randomness
   function _generateRandomDna(string memory _str) private view returns (uint) {
     require(ownerMonkeyCount[msg.sender] == 0);
     uint rand = uint(keccak256(abi.encodePacked(_str)));
--- contracts/PriceConsumerV3.sol
+++ contracts/PriceConsumerV3.sol
@@ -1,6 +1,27 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity ^0.8.0;
 
-import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol"
+import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
 
-contract PriceConsumerV3 {}
+contract PriceConsumerV3 {
+  AggregatorV3Interface public priceFeed;
+
+  constructor() public {
+    // Chainlink Oracle for ETH/USD
+    // Sepolia: 0x694AA1769357215DE4FAC081bf1f309aDC325306
+    // Main net: 0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419
+    priceFeed = AggregatorV3Interface(
+      0x694AA1769357215DE4FAC081bf1f309aDC325306
+    );
+  }
+
+  function getLatestPrice() public view returns (int) {
+    (, int price, , , ) = priceFeed.latestRoundData();
+    return price;
+  }
+
+  function getDecimals() public view returns (uint8) {
+    uint8 decimals = priceFeed.decimals();
+    return decimals;
+  }
+}
